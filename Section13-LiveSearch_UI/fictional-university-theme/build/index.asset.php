a8e149ebf31ea961b8a8dbfc24c0664686da1e25
Somniac2103 <salmon.barry1@gmail.com>
2023-06-24 10:07:22 +0700

    Wordpress Developor course section completion

---
+++ Section13-LiveSearch_UI/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '2397e3d7227861eeb3c8');
---
+++ Section14-WordPressRestAPI/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '487b0b5b913afc79c856');
