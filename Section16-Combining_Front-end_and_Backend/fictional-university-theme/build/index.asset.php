da6b33feba7c6f21ca6158b698528f834202aba4
Somniac2103 <salmon.barry1@gmail.com>
2023-07-10 10:21:43 +0700

    WordPress Developer course Section 16 completed

---
+++ Section16-Combining_Front-end_and_Backend/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '1b63fb8dbc6737460ed8');
---
+++ Section16-Combining_Front-end_and_Backend/images/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => 'b1f230dabffca12e0e5de279687ce551');
