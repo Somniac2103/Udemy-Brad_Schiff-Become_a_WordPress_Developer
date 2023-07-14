c96ce6f85f51d2065c9a7d44513931dbb23e6bba
Somniac2103 <salmon.barry1@gmail.com>
2023-07-14 09:55:20 +0700

    Completed section 18 on users and their premissions

---
+++ Section18-User_roles_and_Permissions/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5a35755737cf9d4fbee9');
---
+++ Section18-User_roles_and_Permissions/fictional-university-theme/searchform.php
@@ -0,0 +1,9 @@
+<form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
+  <label class="headline headline--medium" for="s"> Perform a new search: </label>
+  <div class="search-form-row">
+    <input placeholder="What are you looking for?" class="s" id="s" type="search" name="s">
+    <input class="search-submit" type="submit" value="Search">
+  </div>
+
+
+</form>
